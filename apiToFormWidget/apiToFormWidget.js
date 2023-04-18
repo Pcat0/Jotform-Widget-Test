03df4c87b0d84d87bd8343a7e9431a6a18101c2a
Patrick Ambli <patrick.ambli@gmail.com>
2023-04-18 16:16:23 -0500

    Update apiToFormWidget.js

--- apiToFormWidget/apiToFormWidget.js
+++ apiToFormWidget/apiToFormWidget.js
@@ -1,3 +1,49 @@
-JFCustomWidget.subscribe("ready", function(){
+
+class ApiToFormWidget {
+  apiQueryField;
+  apiurl = "";
   
+  #query = "";
+  constructor() {
+    this.apiQueryField = document.querySelector("#apiQuery");
+    
+    //get widget settings
+    this.apiurl = JFCustomWidget.getWidgetSetting('QuestionLabel');
+
+    //TODO: set query on change of apiQueryField
+    
+  }
+  
+  get query() {
+    return this.#query;
+  }
+  set query(query){
+    this.apiQueryField = query;
+    this.#query = query;
+  }
+  
+  
+  //TODOL: sendData()
+  
+  
+  sendSubmit() {
+    let msg = {
+      valid: true, //TODO: add valid logic
+      value: query
+    }
+    JFCustomWidget.sendSubmit(msg);
+  }
+  populate(data) {
+    
+  }
+}
+
+
+
+
+JFCustomWidget.subscribe("ready", function(){
+  widget = new ApiToFormWidget();
+  
+  JFCustomWidget.subscribe("submit", widget.sendSubmit);
+  JFCustomWidget.subscribe('populate', widget.populateWidgetData);/*(data)=>{
 });
