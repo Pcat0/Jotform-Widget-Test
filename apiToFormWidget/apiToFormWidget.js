e4bc8e23bbadfeca8b5b4a58f05c61ab1e305181
Patrick <pambli@augeomarketing.com>
2023-05-04 08:01:36 -0500

    added comments

--- apiToFormWidget/apiToFormWidget.js
+++ apiToFormWidget/apiToFormWidget.js
@@ -1,25 +1,26 @@
 
 class ApiToFormWidget {
-  apiQueryField;
-  runQueryButton;
+  apiQueryField = document.querySelector("#apiQuery");;
+  runQueryButton = document.querySelector("#runQuery");
   lastQueryRan = null;
   config = {};
   #query = "";
+
   constructor() {
-    this.apiQueryField = document.querySelector("#apiQuery");
+    //setup event listeners
     this.apiQueryField.addEventListener("change", (event)=>{
       this.populate(event.target.value);
     });
-    this.runQueryButton = document.querySelector("#runQuery");
     this.runQueryButton.addEventListener("click", (event)=>{
       this.runAPIQuery();
     });
+
     //get widget settings
     this.config.apiurl = JFCustomWidget.getWidgetSetting('apiurl');
     this.config.autoRun = JFCustomWidget.getWidgetSetting('autorun') == "Yes";
     this.config.hide = JFCustomWidget.getWidgetSetting('hide') == "Yes";
     this.config.outputFields = JFCustomWidget.getWidgetSetting('outputfields').split("\n");
-    console.log(this.config.autoRun);
+
     
     //hide if widget is set to hidden and we are not in the form builder
     if (!JFCustomWidget.isWidgetOnBuilder() && this.config.hide){
@@ -41,7 +42,7 @@ class ApiToFormWidget {
   
   //TODOL: sendData()
   
-  
+  // Sends data to JotForm when the form is submitted
   sendSubmit() {
     let msg = {
       valid: true, //TODO: add valid logic
@@ -49,6 +50,8 @@ class ApiToFormWidget {
     }
     JFCustomWidget.sendSubmit(msg);
   }
+
+  //recive data from Jotform
   populate(newQuery) {
     this.query = newQuery;
 
@@ -57,29 +60,29 @@ class ApiToFormWidget {
     }
 
   }
-  
+  //
   runAPIQuery(){
     //Don't rerun an identical query
     if (this.lastQueryRan == this.query) return;
 
-    this.lastQueryRan = this.query;
+    this.lastQueryRan = this.query; 
 
     console.log("Query ran");
 
     //TODO: handle errors 
+    //Fetch data from API
     fetch(this.config.apiurl + this.query)
       .then(response=>response.json())
       .then(data=>{
+        //loop over data coventering it to a format Jotform understands 
         let output = this.config.outputFields.map((outputField) => {
           return {
             label: outputField,
             value: data[outputField]
           };
         });
+        //Populate fields with data from the API response 
         JFCustomWidget.setFieldsValueByLabel(output);
-        JFCustomWidget.setFieldsValueByLabel([
-          {label: "question:item"}
-        ]);
       });
     
   }
@@ -92,6 +95,7 @@ let widget;
 JFCustomWidget.subscribe("ready", function() {
   widget = new ApiToFormWidget();
   
+  //subscribe to jotformn events
   JFCustomWidget.subscribe("submit", data=>widget.sendSubmit(data));
   JFCustomWidget.subscribe('populate', data=>widget.populate(data.value));
   
